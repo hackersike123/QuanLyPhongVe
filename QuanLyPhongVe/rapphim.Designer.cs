04aa856150372eb34414415b74064ecf7f66c330
nguyen duy tuan <dungcoquen1000@gmail.com>
2025-09-30 19:43:36 +0700

    update 1

--- QuanLyPhongVe/rapphim.Designer.cs
+++ QuanLyPhongVe/rapphim.Designer.cs
@@ -126,7 +126,7 @@
             this.groupBox1.Size = new System.Drawing.Size(286, 243);
             this.groupBox1.TabIndex = 1;
             this.groupBox1.TabStop = false;
-            this.groupBox1.Text = "groupBox1";
+            this.groupBox1.Text = "Ghế ngồi";
             // 
             // button20
             // 
